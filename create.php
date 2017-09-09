1e0bcfa5872b18f89a95469d63f32e457fe23830
Guillaume <dussartguillaume@orange.fr>
2017-09-09 21:13:49 +0200

    add create.php

---
+++ create.php
@@ -0,0 +1,18 @@
+<form method="post">
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp" placeholder="Enter title">
+    </div>
+    <div class="form-group">
+        <label for="content">Content</label>
+        <textarea class="form-control" id="content" name="content" placeholder="Content"></textarea>
+    </div>
+    <div class="form-group">
+        <label for="autor">Autor</label>
+        <input type="text" class="form-control" id="autor" name="autor" placeholder="autor">
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlFile1">Example file input</label>
+        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+    </div>
+</form>
