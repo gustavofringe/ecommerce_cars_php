f475a407fa5930ad2532c3301d186d3e286b3139
Guillaume <dussartguillaume@orange.fr>
2017-09-09 17:39:14 +0200

    add config folder

---
+++ config/conf.php
@@ -0,0 +1,12 @@
+<?php
+class Conf{
+    static $debug = 1;
+    static $databases = array(
+        'default' => array(
+            'host'      =>'localhost',
+            'databasename'  =>'',
+            'login'     =>'',
+            'password'  =>''
+        )
+    );
+}
